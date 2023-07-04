8290a816307b108e706024f4d53e4f5f7a8e243a
Alsoto14 <allinsonsotom@gmail.com>
2023-07-03 21:32:28 -0600

    eso mismo

---
+++ main.js
@@ -0,0 +1,13 @@
+const menuprincipal = document.querySelectorAll('.menu-item');
+
+menuprincipal.forEach(function(item){
+   item.addEventListener('mouseover', function(e){
+        const actualItem = document.querySelector('.active');
+        actualItem.classList.remove('active');
+        e.target.classList.add('active');
+   });
+});
+
+
+
+
